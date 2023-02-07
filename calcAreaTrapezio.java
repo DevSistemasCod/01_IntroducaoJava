06777e738302fcfbe58d6f494a1e51df536ffbd2
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-02-07 16:02:42 -0300

    Update calcAreaTrapezio.java

--- calcAreaTrapezio.java
+++ calcAreaTrapezio.java
@@ -3,14 +3,14 @@ package entradaSaidaDeDados;
 import java.util.Scanner;
 
 public class calcAreaTrapezio {
-	public static void main(String[] args) {
-		float area, baseMaior, baseMenor, altura;
+    public static void main(String[] args) {
+	float area, baseMaior, baseMenor, altura;
 		
-		Scanner lerDado = new Scanner(System.in);
-		System.out.println("Digite a Base Maior do trapézio: ");
+	Scanner lerDado = new Scanner(System.in);
+	System.out.println("Digite a Base Maior do trapézio: ");
         baseMaior = lerDado.nextFloat(); 
         
-		System.out.println("Digite a Base Menor do trapézio: ");
+	System.out.println("Digite a Base Menor do trapézio: ");
         baseMenor = lerDado.nextFloat(); 
         
       	System.out.println("Digite a altura do trapézio: ");
@@ -19,9 +19,8 @@ public class calcAreaTrapezio {
       	area = ((baseMaior+baseMenor)*altura)/2;
       	System.out.println("Área do trapezio: ");
       	System.out.println(area);
-      	
-      	
+      	  	
       	lerDado.close();
-	}
+   }
 
 }
