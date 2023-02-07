258651382c7d61d97dede52685451dae77f48b8f
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-02-07 16:06:37 -0300

    Update calcAreaTriangulo.java

--- calcAreaTriangulo.java
+++ calcAreaTriangulo.java
@@ -4,22 +4,19 @@ import java.util.Scanner;
 
 public class calcAreaTriangulo {
 	public static void main(String[] args) {
-		float area, base, altura;
+	  float area, base, altura;
+	  	
+	  Scanner lerDado = new Scanner(System.in);
+	  System.out.println("Digite a Base do Triângulo: ");
+          base = lerDado.nextFloat(); 
+          
+	  System.out.println("Digite a Altura do Triângulo: ");
+	  altura = lerDado.nextFloat(); 
 		
-		Scanner lerDado = new Scanner(System.in);
-		System.out.println("Digite a Base do Triângulo: ");
-        base = lerDado.nextFloat(); 
-        
-		System.out.println("Digite a Altura do Triângulo: ");
-		altura = lerDado.nextFloat(); 
-        
+      	  area = (base*altura)/2;
+      	  System.out.println("Área do Triângulo: ");
+      	  System.out.printf("%.2f",area);
       	
-      	area = (base*altura)/2;
-      	System.out.println("Área do Triângulo: ");
-      	System.out.printf("%.2f",area);
-      	
-      	
-      	lerDado.close();
+      	  lerDado.close();
 	}
-
 }
